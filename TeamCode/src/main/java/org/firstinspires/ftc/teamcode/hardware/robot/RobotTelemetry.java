e8413309081e1eab4a7021688873a8eea86375a0
rt.Tanus <rafaeltanus2@gmail.com>
2025-02-15 21:16:50 -0300

    Implementação do Clean Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/robot/RobotTelemetry.java
@@ -0,0 +1,32 @@
+package org.firstinspires.ftc.teamcode.hardware;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class RobotTelemetry extends OpMode {
+    public void init(){
+
+    }
+
+    public void periodic(){
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /// NÃO USAR NUNCAAAAAAAAAA///
+    public void loop() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/teleops/TeleopWithOOP.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.opmode.teleops;
+
+public class TeleopWithOOP {
+}
